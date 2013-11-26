6a64112779e0606e139446db1660334abb40a3aa
Ubuntu <ubuntu@ip-10-228-63-42.eu-west-1.compute.internal>
2013-11-26 13:38:32 +0000

    Remove mail functionality

--- ssg-hook.js
+++ ssg-hook.js
@@ -7,8 +7,6 @@ var app     = express();
 var queue   = require('queue-async');
 var tasks   = queue(1);
 var spawn   = require('child_process').spawn;
-var email   = require('emailjs/email');
-var mailer  = email.server.connect(config.email);
 
 app.use(express.bodyParser());
 
@@ -57,8 +55,6 @@ app.post('/hooks/ssg/:branch', function(req, res) {
         run(config.scripts.build, params, function(err) {
             if (err) {
                 console.log('Failed to build: ' + data.owner + '/' + data.repo);
-                //send('Your website at ' + data.owner + '/' + data.repo + ' failed to build.', 'Error building site', data);
-
                 if (typeof cb === 'function') cb();
                 return;
             }
@@ -67,16 +63,12 @@ app.post('/hooks/ssg/:branch', function(req, res) {
             run(config.scripts.publish, params, function(err) {
                 if (err) {
                     console.log('Failed to publish: ' + data.owner + '/' + data.repo);
-                    //send('Your website at ' + data.owner + '/' + data.repo + ' failed to publish.', 'Error publishing site', data);
-
                     if (typeof cb === 'function') cb();
                     return;
                 }
 
                 // Done running scripts
                 console.log('Successfully rendered: ' + data.owner + '/' + data.repo);
-                //send('Your website at ' + data.owner + '/' + data.repo + ' was succesfully published.', 'Succesfully published site', data);
-
                 if (typeof cb === 'function') cb();
                 return;
             });
@@ -106,14 +98,3 @@ function run(file, params, cb) {
     });
 }
 
-function send(body, subject, data) {
-    if (config.email && data.pusher.email) {
-        var message = {
-            text: body,
-            from: config.email.user,
-            to: data.pusher.email,
-            subject: subject
-        };
-        mailer.send(message, function(err) { if (err) console.warn(err); });
-    }
-}
